36e254935d5c7521fa72df957dcdad1adfb05bd6
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2021-04-26 03:49:40 +0300

    Fromtend comlete version

--- frontend/src/components/MyForm/MyForm.js
+++ frontend/src/components/MyForm/MyForm.js
@@ -27,15 +27,17 @@ export default class MyForm extends Component {
       .get(`http://127.0.0.1:5000/showScore`, {
         params: {
           kimlik_no: this.state.kimlik_no,
+          aylik_gelir: this.state.aylik_gelir,
         },
       })
       .then((response) => {
+        console.log(response.data);
         this.setState({ final_score: response.data });
       });
   };
 
   myChangeHandler = (event) => {
-    let nam = event.target.name;
+    let nam = event.target.id;
     let val = event.target.value;
     this.setState({ [nam]: val });
   };
@@ -53,32 +55,53 @@ export default class MyForm extends Component {
     return (
       <Container>
         <h2 style={{ marginBottom: "3rem" }}>SKOR HESAPLAMA UYGULAMASI</h2>
+        {this.state.final_score !== 0 ? (
+          <div>
+            <h3>Your score is: {this.state.final_score}</h3>
+          </div>
+        ) : (
+          <div>
+            <h3>Bilgilerinizi giriniz</h3>
+          </div>
+        )}
         <Form onSubmit={this.mySubmitHandler}>
-          <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Group controlId="kimlik_no">
             <Form.Label>Kimlik No</Form.Label>
-            <Form.Control type="number" placeholder="1234" />
+            <Form.Control
+              type="number"
+              placeholder="1234"
+              onChange={this.myChangeHandler}
+            />
           </Form.Group>
-          <Form.Group controlId="exampleForm.ControlInput2">
+          <Form.Group controlId="ad_soyad">
             <Form.Label>Ad Soyad</Form.Label>
-            <Form.Control type="string" placeholder="ahmet yilmaz" />
+            <Form.Control
+              type="string"
+              placeholder="ahmet yilmaz"
+              onChange={this.myChangeHandler}
+            />
           </Form.Group>
-          <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Group controlId="aylik_gelir">
             <Form.Label>Aylik Gelir</Form.Label>
-            <Form.Control as="select">
+            <Form.Control as="select" onChange={this.myChangeHandler}>
               <option>0-2999TL</option>
-              <option>3000-4999TL</option>
-              <option>5000-7999TL</option>
-              <option>8000-11999TL</option>
-              <option>12000 ve üzeri</option>
+              <option>3000TL-4999TL</option>
+              <option>5000TL-7999TL</option>
+              <option>8000TL-11999TL</option>
+              <option>12000TL ve üzeri</option>
             </Form.Control>
           </Form.Group>
-          <Form.Group controlId="exampleForm.ControlInput3">
+          <Form.Group controlId="telefon">
             <Form.Label>Telefon</Form.Label>
-            <Form.Control type="number" placeholder="0534" />
+            <Form.Control
+              type="number"
+              placeholder="0534"
+              onChange={this.myChangeHandler}
+            />
           </Form.Group>
-          <Form.Group controlId="exampleForm.ControlSelect2">
+          <Form.Group controlId="ikamet_ili">
             <Form.Label>İkamet Edilen İl</Form.Label>
-            <Form.Control as="select" multiple>
+            <Form.Control as="select" multiple onChange={this.myChangeHandler}>
               {this.createOptionItems()}
             </Form.Control>
           </Form.Group>
@@ -86,11 +109,6 @@ export default class MyForm extends Component {
             Hesapla
           </Button>
         </Form>
-        {this.state.final_score != 0 ?? (
-          <div>
-            <h3>Your score is: {this.state.final_score}</h3>
-          </div>
-        )}
       </Container>
     );
   }
